1fab31e95aa9107e79496649a24241ed6b45584f
gmartins9042 <gmartins9042@gmail.com>
2026-06-07 18:05:17 -0300

    feat: add scanner and menu option variable

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class ContaBancaria {
 
     String name = "Gabriel Martins";
     String account = "corrente";
     double balance = 5000;
+    Scanner reader = new Scanner(System.in);
+    int option = 0;
 }
