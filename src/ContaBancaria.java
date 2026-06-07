9db965480fe18759e1cebb26b544beeb7189d333
gmartins9042 <gmartins9042@gmail.com>
2026-06-07 18:08:24 -0300

    feat: display customer account details

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -1,10 +1,22 @@
 import java.util.Scanner;
 
 public class ContaBancaria {
+    public static void main(String[] args) {
 
-    String name = "Gabriel Martins";
-    String account = "corrente";
-    double balance = 5000;
-    Scanner reader = new Scanner(System.in);
-    int option = 0;
+        String name = "Gabriel Martins";
+        String account = "corrente";
+        double balance = 5000;
+        Scanner reader = new Scanner(System.in);
+        int option = 0;
+
+        System.out.println("""
+                *************************************
+                Dados iniciais do cliente:
+                
+                Nome: %s
+                Tipo conta: %s
+                Saldo inicial: R$: %.2f
+                *************************************
+                """.formatted(name, account, balance));
+    }
 }
